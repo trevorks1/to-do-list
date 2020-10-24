02f77f948afac4c677334df22a7ad3b9bdd2b9b9
Trevor Sansalone <trevorksansalone@gmail.com>
2020-10-24 13:26:40 -0500

    got my get and post running in basic form

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -5,8 +5,14 @@ const pool = require('../modules/pool.js');
 // DB connection
 
 // GET
+toDoRouter.get('/', (req, res) => {
+  res.send('Get Hello');
+});
 
 // POST
+toDoRouter.post('/', (req, res) => {
+  res.sendStatus(201);
+});
 
 // PUT
 
